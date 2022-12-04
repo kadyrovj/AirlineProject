44577e1f0809c2246d69f1f871da71d94f4a8342
IgorChicago <116406707+IgorChicago@users.noreply.github.com>
2022-12-04 14:04:41 -0600

    Luggage Price

--- src/com/java/AirlineProject/MainClass.java
+++ src/com/java/AirlineProject/MainClass.java
@@ -5,6 +5,10 @@ public class MainClass {
         double totalPrice = 0;
         Jasur ar = new Jasur();
         Chimita fl = new Chimita();
+        Luggage lu = new Luggage(); // luggage class where I calculated the luggage price. Igor
+
+        totalPrice = totalPrice+ lu.additionToTicketPrice; // added luggage price to the total price
+
         ar.confirmPurchase(2);
         fl.directOrConnectingFlight(totalPrice);
 
