227a22039e48077cbe3189f3894d22714134d092
Viktoriia <izhovska.viktoriia@gmail.com>
2022-12-04 05:31:01 +0200

    added method checkAnswer
    that checks whether the user entered correctly YES or NO answer

--- src/com/java/AirlineProject/Check.java
+++ src/com/java/AirlineProject/Check.java
@@ -92,4 +92,13 @@ public class Check {
         }
         return Integer.parseInt(intStr);
     }
+    //checks if the entered answer is YES or NO
+    public  String checkAnswer (Scanner sc){
+        String answer = sc.nextLine();
+        while (!(answer.equalsIgnoreCase("yes") || answer.equalsIgnoreCase("no"))){
+            System.out.println("Your input is incorrect! Please enter YES or NO:");
+            answer = sc.nextLine();
+        }
+        return answer;
+    }
 }
